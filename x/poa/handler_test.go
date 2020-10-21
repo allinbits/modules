8ff05b5e54008f3695f92e45233bea226ca84cbb
PaddyMc <paddymchale@hotmail.com>
2020-10-21 17:54:19 +0100

    chore(test): remove unused test util

--- x/poa/handler_test.go
+++ x/poa/handler_test.go
@@ -1,12 +1,7 @@
 package poa
 
 import (
-	"bytes"
-	"encoding/hex"
 	"testing"
-
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/crypto/ed25519"
 
 	"github.com/allinbits/modules/x/poa/keeper"
 	"github.com/allinbits/modules/x/poa/msg"
@@ -16,35 +11,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: Should be able to import SamplePubKey & MakeTestPubKey from keeper
-// TODO: for some reason Go is complaining here? Any ideas?
-
-// ----------------------------------------------------
-
-const (
-	SamplePubKey = "b7a3c12dc0c8c748ab07525b701122b88bd78f600c76342d27f25e5f92444cde"
-)
-
-func MakeTestPubKey(pk string) (res crypto.PubKey) {
-	var buffer bytes.Buffer
-	buffer.WriteString(pk)
-
-	pkBytes, err := hex.DecodeString(buffer.String())
-	if err != nil {
-		panic(err)
-	}
-	var pkEd ed25519.PubKeyEd25519
-	copy(pkEd[:], pkBytes)
-	return pkEd
-}
-
-// ----------------------------------------------------
-
 func TestHandleCreateValidatorPOA(t *testing.T) {
-	ctx, keeper := keeper.MakeTestCtxAndKeeper(t)
+	ctx, k := keeper.MakeTestCtxAndKeeper(t)
 
 	name := "name"
-	valPubKey := MakeTestPubKey(SamplePubKey)
+	valPubKey := keeper.MakeTestPubKey(keeper.SamplePubKey)
 	valAddr := sdk.ValAddress(valPubKey.Address().Bytes())
 	accAddr := sdk.AccAddress(valPubKey.Address().Bytes())
 
@@ -56,10 +27,10 @@ func TestHandleCreateValidatorPOA(t *testing.T) {
 		accAddr,
 	)
 
-	res, err := handleMsgCreateValidatorPOA(ctx, msg, keeper)
+	res, err := handleMsgCreateValidatorPOA(ctx, msg, k)
 
 	// Validate msg was handled correctly but checking the size of the store
-	allVals := keeper.GetAllValidators(ctx)
+	allVals := k.GetAllValidators(ctx)
 	require.Equal(t, 1, len(allVals))
 
 	// No errors and res is populates
@@ -68,10 +39,10 @@ func TestHandleCreateValidatorPOA(t *testing.T) {
 }
 
 func TestHandleVoteValidatorPOA(t *testing.T) {
-	ctx, keeper := keeper.MakeTestCtxAndKeeper(t)
+	ctx, k := keeper.MakeTestCtxAndKeeper(t)
 
 	name := "name"
-	valPubKey := MakeTestPubKey(SamplePubKey)
+	valPubKey := keeper.MakeTestPubKey(keeper.SamplePubKey)
 	valAddr := sdk.ValAddress(valPubKey.Address().Bytes())
 	accAddr := sdk.AccAddress(valPubKey.Address().Bytes())
 
@@ -82,16 +53,16 @@ func TestHandleVoteValidatorPOA(t *testing.T) {
 		stakingtypes.Description{"nil", "nil", "nil", "nil", "nil"},
 	)
 
-	keeper.SetValidator(ctx, name, validator)
-	keeper.CalculateValidatorVotes(ctx)
-	keeper.ApplyAndReturnValidatorSetUpdates(ctx)
+	k.SetValidator(ctx, name, validator)
+	k.CalculateValidatorVotes(ctx)
+	k.ApplyAndReturnValidatorSetUpdates(ctx)
 
 	msg := msg.NewMsgVoteValidator("name", valAddr, true, accAddr)
 
-	res, err := handleMsgVoteValidator(ctx, msg, keeper)
+	res, err := handleMsgVoteValidator(ctx, msg, k)
 
 	// Validate msg was handled correctly but checking the size of the store
-	allVotes := keeper.GetAllVotes(ctx)
+	allVotes := k.GetAllVotes(ctx)
 	require.Equal(t, 1, len(allVotes))
 
 	// No errors and res is populated
